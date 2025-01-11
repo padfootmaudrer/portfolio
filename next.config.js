dd084c218c72b9e162127af26c2a13a7287ea62c
Sushobhith Sharma <padfoot@Sushobhiths-Air.ht.home>
2025-01-11 16:33:29 -0600

    Still fixing

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  // images: {
+  //   // Example custom config
+  //   domains: ['example.com'],
+  //   unoptimized: false,
+  // },
+};
+  
+module.exports = nextConfig;
+  
